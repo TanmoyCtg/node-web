736079ebd36188a01a55c6f0024c5ed6d76f6738
Tanmoyctg <chowdhuryiftekharulislam@outlook.com>
2019-05-05 19:09:37 +0600

    modified

--- server.js
+++ server.js
@@ -82,12 +82,12 @@ app.get('/bad', (req, res)=>{
 
 
 // Constants
-const PORT = 8080;
+const port = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
 
 
-app.listen(PORT, HOST);
+app.listen(port, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 
